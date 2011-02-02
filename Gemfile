6bbbb7b88983a744dec93cb5766b146afe2cd11d
wiseadvise <wiseadvise.development@gmail.com>
2011-02-02 21:52:03 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'unicorn'
+gem 'mysql2'
+gem 'rqrcode'
+gem 'gd2'
+
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubber::Application.config.secret_token = '9196281d3140942b6a4e4db81cf305c240192c9596bcc52e515d075d62b14c2ed6c3ecf3e507497d850ed1603a789d180fa72702c5c562f7b1a3a6575f66f5cf'
